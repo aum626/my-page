a2a444d1b1487714ad87425a1f9a8ac8f1504959
aum626 <aum626@MacBook-Air.local>
2025-10-16 21:06:08 +0700

    16/10/2568

--- src/blog/blog.js
+++ src/blog/blog.js
@@ -1,13 +1,22 @@
 import { Helmet } from 'react-helmet';
+import "./blog.css"
 const Blog=()=>{
     return(
         <section>
             <Helmet>
                 <title>Blog</title>
             </Helmet>
-            <div className="container">
+            <div className='head'>
                 <h1>Blog</h1>
                 <p>Welcome to the blog section!</p>
+            </div>
+            <div className="container">
+                <div className='boxBlog'>
+                    <img src="./logo192.png" alt='/' />
+                    <div className='data'>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum beatae doloribus eaque porro libero harum molestiae animi aperiam laudantium, iste nemo nostrum quam atque possimus magnam vel quo alias sit sequi in facere deserunt quidem! Voluptas, deserunt sapiente aut voluptatem id repellat nemo cupiditate blanditiis eaque velit nam accusantium itaque ipsam tenetur nulla earum libero recusandae laborum hic necessitatibus laboriosam. Dolorum nobis nostrum numquam dolorem modi, beatae laborum rem esse aliquam omnis adipisci dolore labore voluptates laudantium doloribus pariatur unde quos praesentium, inventore sapiente dolor! Aut, nobis. Aliquid eum eveniet minus sunt, sapiente suscipit itaque hic nam tenetur. Deleniti nesciunt eum, eveniet perspiciatis totam delectus deserunt, iusto assumenda recusandae dolor fuga quo nisi maxime sapiente, neque impedit ipsa similique sed! Molestiae iure similique est nihil? Quod ut earum quia. Necessitatibus officia perferendis dolor rem neque dignissimos facere assumenda impedit cumque quidem a quos itaque exercitationem, nulla porro aspernatur unde tempore, deleniti, consectetur mollitia. Omnis atque quas, at distinctio culpa illum expedita alias optio eius voluptatem facilis illo aperiam, dicta quia quam incidunt. Odit tempora velit veniam accusantium provident reprehenderit natus itaque repellat ea consequuntur aliquid a, praesentium, id laboriosam vel assumenda autem perferendis fugit, obcaecati magnam adipisci saepe ipsum? A, magni, qui vitae nisi officiis impedit nobis error dolor aspernatur aut repudiandae quam? Suscipit assumenda officiis architecto, eius corporis pariatur ea corrupti dolorum quam inventore sed ullam voluptatibus nemo! Delectus sunt voluptatem maxime? Quo, maiores magnam dolor sunt vero atque. Nisi explicabo quod necessitatibus vero mollitia nulla architecto vitae. Tempore at ex quidem! Tempora iure odit dicta obcaecati dolorum iste ipsa voluptate ea nisi molestiae minus facere, animi aspernatur commodi suscipit recusandae corrupti et, accusantium accusamus! Ex ut iure corrupti fuga blanditiis autem illum voluptates aliquam voluptatibus placeat tenetur, magni debitis doloribus aliquid soluta cupiditate reprehenderit. Voluptatem, aut. Fugiat, consequatur.</p>
+                    </div>
+                </div>
             </div>
         </section>
     )
